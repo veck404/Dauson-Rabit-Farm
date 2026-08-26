4154a1313fb93cf7195ae8aa7f3d54399c72fefc
veck404 <victor7ultimate@gmail.com>
2026-08-26 10:54:26 +0100

    tr

--- lib/farm-utils.ts
+++ lib/farm-utils.ts
@@ -16,7 +16,7 @@ export const shortDate = (value: string) =>
   new Intl.DateTimeFormat("en-NG", {
     day: "2-digit",
     month: "short",
-    year: "numeric",
+    year: "numerc",
   }).format(new Date(`${value}T12:00:00`));
 
 export const daysUntil = (value: string) =>
